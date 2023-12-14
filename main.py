bd87afc3623a51d965ed00e808ccb5587471e162
ElielNicollas <119982627+ElielM10100@users.noreply.github.com>
2023-12-14 19:29:38 -0300

    Add files via upload
    Escreva uma função que leia base e altura de um triangulo e imprima a sua área
    area = base * altura / 2

---
+++ main.py
@@ -0,0 +1,6 @@
+def areaTriangulo():
+    base = float(input('Digite a base: '))
+    altura = float(input('Digite a altura: '))
+    area = base * altura / 2
+    print(f"Área do triangulo = {area}")
+areaTriangulo()
